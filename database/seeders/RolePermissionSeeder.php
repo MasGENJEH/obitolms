9193681b0384f0eb34c3eb4171021c09fc28489e
Fachri Ibnu Falah <fahriibnu1234@gmail.com>
2026-01-08 20:01:22 +0700

    [FEATURE] membuat seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::create([
+            'name' => 'admin',
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student',
+        ]);
+
+        $mentorRole = Role::create([
+            'name' => 'mentor',
+        ]);
+
+        $user = User::create([
+            'name' => 'Fufu',
+            'email' => 'fufu@test.com',
+            'password' => bcrypt('1234512345'),
+        ]);
+
+        $user->assignRole($adminRole);
+    }
+}
