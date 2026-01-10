7091bd8c5bd86fd0b14b427da79ba79f0124f5a3
Fachri Ibnu Falah <fahriibnu1234@gmail.com>
2026-01-10 17:45:01 +0700

    [FEATURE] Menambahkan view transaction

--- app/Filament/Resources/Transactions/Tables/TransactionsTable.php
+++ app/Filament/Resources/Transactions/Tables/TransactionsTable.php
@@ -7,6 +7,9 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -16,7 +19,18 @@ class TransactionsTable
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('student.photo'),
+                TextColumn::make('student.name')->searchable()->sortable(),
+                TextColumn::make('booking_trx_id')->searchable(),
+                TextColumn::make('pricing.name'),
+                TextColumn::make('created_at'),
+                IconColumn::make('is_paid')
+                ->boolean()
+                ->trueColor('success')
+                ->falseColor('danger')
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->label('Terverifikasi'),
             ])
             ->filters([
                 TrashedFilter::make(),
